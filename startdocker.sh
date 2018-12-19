1443faa0a3e1d1cae68411c8ffec7377e597cd24
zhs007 <sssxueren@gmail.com>
2018-12-19 18:00:46 +0800

    upd

--- startdocker.sh
+++ startdocker.sh
@@ -1,3 +1,9 @@
-docker container stop jarvistviewer
-docker container rm jarvistviewer
-docker run -d --name jarvistviewer -v $PWD/cfg:/home/jarvistviewer/cfg -v $PWD/logs:/home/jarvistviewer/logs -v $PWD/dat:/home/jarvistviewer/dat jarvistviewer
+docker container stop jarvisviewer
+docker container rm jarvisviewer
+docker run -d --name jarvisviewer \
+    -v $PWD/cfg:/home/jarvisviewer/cfg \
+    -v $PWD/logs:/home/jarvisviewer/logs \ 
+    -v $PWD/dat:/home/jarvisviewer/dat \
+    -p 7710:7710 \
+    -p 7788:7788 \
+    jarvisviewer
