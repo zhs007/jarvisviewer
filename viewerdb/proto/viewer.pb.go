cf1b24ce5df23ce924f5bb87d4a265e35953c4d3
zhs007 <sssxueren@gmail.com>
2018-12-20 18:16:12 +0800

    add line

--- viewerdb/proto/viewer.pb.go
+++ viewerdb/proto/viewer.pb.go
@@ -18,36 +18,47 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
+// ViewerType - Viewer type
+//            - 数据类型
 type ViewerType int32
 
 const (
 	ViewerType_EMPTY ViewerType = 0
 	ViewerType_GRAPH ViewerType = 1
 	ViewerType_JSON  ViewerType = 2
+	ViewerType_LINE  ViewerType = 3
 )
 
 var ViewerType_name = map[int32]string{
 	0: "EMPTY",
 	1: "GRAPH",
 	2: "JSON",
+	3: "LINE",
 }
 var ViewerType_value = map[string]int32{
 	"EMPTY": 0,
 	"GRAPH": 1,
 	"JSON":  2,
+	"LINE":  3,
 }
 
 func (x ViewerType) String() string {
 	return proto.EnumName(ViewerType_name, int32(x))
 }
 func (ViewerType) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_viewer_8092c8b6e5f0756f, []int{0}
+	return fileDescriptor_viewer_b865312303b538d4, []int{0}
 }
 
+// GraphNode - Graph node
+//           - 图节点
 type GraphNode struct {
-	Id                   int32    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
-	Name                 string   `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
-	Size                 int32    `protobuf:"varint,3,opt,name=size,proto3" json:"size,omitempty"`
+	// id
+	Id int32 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	// name
+	Name string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	// size
+	Size int32 `protobuf:"varint,3,opt,name=size,proto3" json:"size,omitempty"`
+	// category
 	Category             string   `protobuf:"bytes,4,opt,name=category,proto3" json:"category,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -58,7 +69,7 @@ func (m *GraphNode) Reset()         { *m = GraphNode{} }
 func (m *GraphNode) String() string { return proto.CompactTextString(m) }
 func (*GraphNode) ProtoMessage()    {}
 func (*GraphNode) Descriptor() ([]byte, []int) {
-	return fileDescriptor_viewer_8092c8b6e5f0756f, []int{0}
+	return fileDescriptor_viewer_b865312303b538d4, []int{0}
 }
 func (m *GraphNode) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GraphNode.Unmarshal(m, b)
@@ -106,9 +117,14 @@ func (m *GraphNode) GetCategory() string {
 	return ""
 }
 
+// GraphLink - Graph link info
+//           - 图节点连接关系
 type GraphLink struct {
-	Src                  int32    `protobuf:"varint,1,opt,name=src,proto3" json:"src,omitempty"`
-	Dest                 int32    `protobuf:"varint,2,opt,name=dest,proto3" json:"dest,omitempty"`
+	// src
+	Src int32 `protobuf:"varint,1,opt,name=src,proto3" json:"src,omitempty"`
+	// dest
+	Dest int32 `protobuf:"varint,2,opt,name=dest,proto3" json:"dest,omitempty"`
+	// size
 	Size                 int32    `protobuf:"varint,3,opt,name=size,proto3" json:"size,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -119,7 +135,7 @@ func (m *GraphLink) Reset()         { *m = GraphLink{} }
 func (m *GraphLink) String() string { return proto.CompactTextString(m) }
 func (*GraphLink) ProtoMessage()    {}
 func (*GraphLink) Descriptor() ([]byte, []int) {
-	return fileDescriptor_viewer_8092c8b6e5f0756f, []int{1}
+	return fileDescriptor_viewer_b865312303b538d4, []int{1}
 }
 func (m *GraphLink) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GraphLink.Unmarshal(m, b)
@@ -160,8 +176,12 @@ func (m *GraphLink) GetSize() int32 {
 	return 0
 }
 
+// GraphData - Graph data
+//           - 图节点完整数据
 type GraphData struct {
-	Nodes                []*GraphNode `protobuf:"bytes,1,rep,name=nodes,proto3" json:"nodes,omitempty"`
+	// nodes
+	Nodes []*GraphNode `protobuf:"bytes,1,rep,name=nodes,proto3" json:"nodes,omitempty"`
+	// links
 	Links                []*GraphLink `protobuf:"bytes,2,rep,name=links,proto3" json:"links,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
 	XXX_unrecognized     []byte       `json:"-"`
@@ -172,7 +192,7 @@ func (m *GraphData) Reset()         { *m = GraphData{} }
 func (m *GraphData) String() string { return proto.CompactTextString(m) }
 func (*GraphData) ProtoMessage()    {}
 func (*GraphData) Descriptor() ([]byte, []int) {
-	return fileDescriptor_viewer_8092c8b6e5f0756f, []int{2}
+	return fileDescriptor_viewer_b865312303b538d4, []int{2}
 }
 func (m *GraphData) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GraphData.Unmarshal(m, b)
@@ -206,7 +226,10 @@ func (m *GraphData) GetLinks() []*GraphLink {
 	return nil
 }
 
+// JSONData - JSON data
+//          - json格式的数据
 type JSONData struct {
+	// string for json
 	Str                  string   `protobuf:"bytes,1,opt,name=str,proto3" json:"str,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -217,7 +240,7 @@ func (m *JSONData) Reset()         { *m = JSONData{} }
 func (m *JSONData) String() string { return proto.CompactTextString(m) }
 func (*JSONData) ProtoMessage()    {}
 func (*JSONData) Descriptor() ([]byte, []int) {
-	return fileDescriptor_viewer_8092c8b6e5f0756f, []int{3}
+	return fileDescriptor_viewer_b865312303b538d4, []int{3}
 }
 func (m *JSONData) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_JSONData.Unmarshal(m, b)
@@ -244,13 +267,135 @@ func (m *JSONData) GetStr() string {
 	return ""
 }
 
+// LineSeriesData - Line series data
+//                - 折线图数据
+type LineSeriesData struct {
+	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Val                  []int32  `protobuf:"varint,2,rep,packed,name=val,proto3" json:"val,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *LineSeriesData) Reset()         { *m = LineSeriesData{} }
+func (m *LineSeriesData) String() string { return proto.CompactTextString(m) }
+func (*LineSeriesData) ProtoMessage()    {}
+func (*LineSeriesData) Descriptor() ([]byte, []int) {
+	return fileDescriptor_viewer_b865312303b538d4, []int{4}
+}
+func (m *LineSeriesData) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_LineSeriesData.Unmarshal(m, b)
+}
+func (m *LineSeriesData) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_LineSeriesData.Marshal(b, m, deterministic)
+}
+func (dst *LineSeriesData) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LineSeriesData.Merge(dst, src)
+}
+func (m *LineSeriesData) XXX_Size() int {
+	return xxx_messageInfo_LineSeriesData.Size(m)
+}
+func (m *LineSeriesData) XXX_DiscardUnknown() {
+	xxx_messageInfo_LineSeriesData.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LineSeriesData proto.InternalMessageInfo
+
+func (m *LineSeriesData) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+func (m *LineSeriesData) GetVal() []int32 {
+	if m != nil {
+		return m.Val
+	}
+	return nil
+}
+
+// LineData - Line data
+//           - 折线图完整数据
+type LineData struct {
+	// xAxisType
+	XAxisType string `protobuf:"bytes,1,opt,name=xAxisType,proto3" json:"xAxisType,omitempty"`
+	// xAxisData
+	XAxisData []string `protobuf:"bytes,2,rep,name=xAxisData,proto3" json:"xAxisData,omitempty"`
+	// yAxisType
+	YAxisType string `protobuf:"bytes,3,opt,name=yAxisType,proto3" json:"yAxisType,omitempty"`
+	// xAxisData
+	Series               []*LineSeriesData `protobuf:"bytes,4,rep,name=series,proto3" json:"series,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
+	XXX_unrecognized     []byte            `json:"-"`
+	XXX_sizecache        int32             `json:"-"`
+}
+
+func (m *LineData) Reset()         { *m = LineData{} }
+func (m *LineData) String() string { return proto.CompactTextString(m) }
+func (*LineData) ProtoMessage()    {}
+func (*LineData) Descriptor() ([]byte, []int) {
+	return fileDescriptor_viewer_b865312303b538d4, []int{5}
+}
+func (m *LineData) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_LineData.Unmarshal(m, b)
+}
+func (m *LineData) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_LineData.Marshal(b, m, deterministic)
+}
+func (dst *LineData) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LineData.Merge(dst, src)
+}
+func (m *LineData) XXX_Size() int {
+	return xxx_messageInfo_LineData.Size(m)
+}
+func (m *LineData) XXX_DiscardUnknown() {
+	xxx_messageInfo_LineData.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LineData proto.InternalMessageInfo
+
+func (m *LineData) GetXAxisType() string {
+	if m != nil {
+		return m.XAxisType
+	}
+	return ""
+}
+
+func (m *LineData) GetXAxisData() []string {
+	if m != nil {
+		return m.XAxisData
+	}
+	return nil
+}
+
+func (m *LineData) GetYAxisType() string {
+	if m != nil {
+		return m.YAxisType
+	}
+	return ""
+}
+
+func (m *LineData) GetSeries() []*LineSeriesData {
+	if m != nil {
+		return m.Series
+	}
+	return nil
+}
+
+// ViewerData - viewer data
+//            - viewer数据
 type ViewerData struct {
-	Type  ViewerType `protobuf:"varint,1,opt,name=type,proto3,enum=viewerdbpb.ViewerType" json:"type,omitempty"`
-	Title string     `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
-	Token string     `protobuf:"bytes,3,opt,name=token,proto3" json:"token,omitempty"`
+	// type
+	Type ViewerType `protobuf:"varint,1,opt,name=type,proto3,enum=viewerdbpb.ViewerType" json:"type,omitempty"`
+	// title
+	Title string `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
+	// token
+	Token string `protobuf:"bytes,3,opt,name=token,proto3" json:"token,omitempty"`
 	// Types that are valid to be assigned to Data:
 	//	*ViewerData_Graph
 	//	*ViewerData_Json
+	//	*ViewerData_Line
 	Data                 isViewerData_Data `protobuf_oneof:"data"`
 	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
 	XXX_unrecognized     []byte            `json:"-"`
@@ -261,7 +406,7 @@ func (m *ViewerData) Reset()         { *m = ViewerData{} }
 func (m *ViewerData) String() string { return proto.CompactTextString(m) }
 func (*ViewerData) ProtoMessage()    {}
 func (*ViewerData) Descriptor() ([]byte, []int) {
-	return fileDescriptor_viewer_8092c8b6e5f0756f, []int{4}
+	return fileDescriptor_viewer_b865312303b538d4, []int{6}
 }
 func (m *ViewerData) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ViewerData.Unmarshal(m, b)
@@ -291,9 +436,13 @@ type ViewerData_Graph struct {
 type ViewerData_Json struct {
 	Json *JSONData `protobuf:"bytes,1001,opt,name=json,proto3,oneof"`
 }
+type ViewerData_Line struct {
+	Line *LineData `protobuf:"bytes,1002,opt,name=line,proto3,oneof"`
+}
 
 func (*ViewerData_Graph) isViewerData_Data() {}
 func (*ViewerData_Json) isViewerData_Data()  {}
+func (*ViewerData_Line) isViewerData_Data()  {}
 
 func (m *ViewerData) GetData() isViewerData_Data {
 	if m != nil {
@@ -337,11 +486,19 @@ func (m *ViewerData) GetJson() *JSONData {
 	return nil
 }
 
+func (m *ViewerData) GetLine() *LineData {
+	if x, ok := m.GetData().(*ViewerData_Line); ok {
+		return x.Line
+	}
+	return nil
+}
+
 // XXX_OneofFuncs is for the internal use of the proto package.
 func (*ViewerData) XXX_OneofFuncs() (func(msg proto.Message, b *proto.Buffer) error, func(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error), func(msg proto.Message) (n int), []interface{}) {
 	return _ViewerData_OneofMarshaler, _ViewerData_OneofUnmarshaler, _ViewerData_OneofSizer, []interface{}{
 		(*ViewerData_Graph)(nil),
 		(*ViewerData_Json)(nil),
+		(*ViewerData_Line)(nil),
 	}
 }
 
@@ -357,6 +514,11 @@ func _ViewerData_OneofMarshaler(msg proto.Message, b *proto.Buffer) error {
 	case *ViewerData_Json:
 		b.EncodeVarint(1001<<3 | proto.WireBytes)
 		if err := b.EncodeMessage(x.Json); err != nil {
+			return err
+		}
+	case *ViewerData_Line:
+		b.EncodeVarint(1002<<3 | proto.WireBytes)
+		if err := b.EncodeMessage(x.Line); err != nil {
 			return err
 		}
 	case nil:
@@ -385,6 +547,14 @@ func _ViewerData_OneofUnmarshaler(msg proto.Message, tag, wire int, b *proto.Buf
 		err := b.DecodeMessage(msg)
 		m.Data = &ViewerData_Json{msg}
 		return true, err
+	case 1002: // data.line
+		if wire != proto.WireBytes {
+			return true, proto.ErrInternalBadWireType
+		}
+		msg := new(LineData)
+		err := b.DecodeMessage(msg)
+		m.Data = &ViewerData_Line{msg}
+		return true, err
 	default:
 		return false, nil
 	}
@@ -404,6 +574,11 @@ func _ViewerData_OneofSizer(msg proto.Message) (n int) {
 		n += 2 // tag and wire
 		n += proto.SizeVarint(uint64(s))
 		n += s
+	case *ViewerData_Line:
+		s := proto.Size(x.Line)
+		n += 2 // tag and wire
+		n += proto.SizeVarint(uint64(s))
+		n += s
 	case nil:
 	default:
 		panic(fmt.Sprintf("proto: unexpected type %T in oneof", x))
@@ -416,35 +591,43 @@ func init() {
 	proto.RegisterType((*GraphLink)(nil), "viewerdbpb.GraphLink")
 	proto.RegisterType((*GraphData)(nil), "viewerdbpb.GraphData")
 	proto.RegisterType((*JSONData)(nil), "viewerdbpb.JSONData")
+	proto.RegisterType((*LineSeriesData)(nil), "viewerdbpb.LineSeriesData")
+	proto.RegisterType((*LineData)(nil), "viewerdbpb.LineData")
 	proto.RegisterType((*ViewerData)(nil), "viewerdbpb.ViewerData")
 	proto.RegisterEnum("viewerdbpb.ViewerType", ViewerType_name, ViewerType_value)
 }
 
-func init() { proto.RegisterFile("viewer.proto", fileDescriptor_viewer_8092c8b6e5f0756f) }
+func init() { proto.RegisterFile("viewer.proto", fileDescriptor_viewer_b865312303b538d4) }
 
-var fileDescriptor_viewer_8092c8b6e5f0756f = []byte{
-	// 354 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x92, 0xc1, 0x4e, 0xea, 0x40,
-	0x14, 0x86, 0x69, 0x69, 0xb9, 0xf4, 0xdc, 0x1b, 0xd2, 0x4c, 0xb8, 0x66, 0x62, 0x5c, 0x34, 0x5d,
-	0x11, 0x30, 0x5d, 0xe0, 0x13, 0x68, 0x24, 0x10, 0xa3, 0x48, 0x46, 0x62, 0xe2, 0xca, 0x14, 0x3a,
-	0xc1, 0x11, 0xec, 0x34, 0xed, 0x44, 0x83, 0x6f, 0xe8, 0x9b, 0xe8, 0x5b, 0x98, 0x73, 0x06, 0x90,
-	0x05, 0xee, 0xfe, 0x73, 0xf2, 0xcd, 0xf9, 0xff, 0xbf, 0x29, 0xfc, 0x7b, 0x55, 0xf2, 0x4d, 0x96,
-	0x49, 0x51, 0x6a, 0xa3, 0x19, 0xd8, 0x29, 0x9b, 0x15, 0xb3, 0xf8, 0x11, 0x82, 0x61, 0x99, 0x16,
-	0x4f, 0x63, 0x9d, 0x49, 0xd6, 0x02, 0x57, 0x65, 0xdc, 0x89, 0x9c, 0x8e, 0x2f, 0x5c, 0x95, 0x31,
-	0x06, 0x5e, 0x9e, 0xbe, 0x48, 0xee, 0x46, 0x4e, 0x27, 0x10, 0xa4, 0x71, 0x57, 0xa9, 0x77, 0xc9,
-	0xeb, 0x44, 0x91, 0x66, 0xc7, 0xd0, 0x9c, 0xa7, 0x46, 0x2e, 0x74, 0xb9, 0xe6, 0x1e, 0xb1, 0xbb,
-	0x39, 0x1e, 0x6c, 0x0c, 0xae, 0x55, 0xbe, 0x64, 0x21, 0xd4, 0xab, 0x72, 0xbe, 0x71, 0x40, 0x89,
-	0xe7, 0x32, 0x59, 0x19, 0xb2, 0xf0, 0x05, 0xe9, 0x43, 0x16, 0xb1, 0xdc, 0x9c, 0xb9, 0x4c, 0x4d,
-	0xca, 0x7a, 0xe0, 0xe7, 0x3a, 0x93, 0x15, 0x77, 0xa2, 0x7a, 0xe7, 0x6f, 0xff, 0x7f, 0xf2, 0x53,
-	0x28, 0xd9, 0xb5, 0x11, 0x96, 0x41, 0x78, 0xa5, 0xf2, 0x65, 0xc5, 0xdd, 0x5f, 0x60, 0x4c, 0x26,
-	0x2c, 0x13, 0x9f, 0x40, 0xf3, 0xea, 0xee, 0x76, 0x4c, 0x2e, 0x18, 0xd6, 0x94, 0x14, 0x36, 0x10,
-	0x28, 0xe3, 0x0f, 0x07, 0xe0, 0x9e, 0x5e, 0x13, 0xd0, 0x05, 0xcf, 0xac, 0x0b, 0x49, 0x44, 0xab,
-	0x7f, 0xb4, 0x7f, 0xd8, 0x52, 0xd3, 0x75, 0x21, 0x05, 0x31, 0xac, 0x0d, 0xbe, 0x51, 0x66, 0xb5,
-	0xfd, 0x96, 0x76, 0xa0, 0xad, 0x5e, 0xca, 0x9c, 0xaa, 0xe2, 0x16, 0x07, 0x96, 0x80, 0xbf, 0xc0,
-	0x60, 0xfc, 0xf3, 0x4f, 0xe4, 0x1c, 0x8c, 0x8c, 0xf6, 0xa3, 0x9a, 0xb0, 0x18, 0xeb, 0x81, 0xf7,
-	0x5c, 0xe9, 0x9c, 0x7f, 0x59, 0xbc, 0xbd, 0x8f, 0x6f, 0xdb, 0x8c, 0x6a, 0x82, 0xa0, 0x8b, 0x06,
-	0x78, 0x59, 0x6a, 0xd2, 0xee, 0xe9, 0xb6, 0x0a, 0x86, 0x64, 0x01, 0xf8, 0x83, 0x9b, 0xc9, 0xf4,
-	0x21, 0xac, 0xa1, 0x1c, 0x8a, 0xf3, 0xc9, 0x28, 0x74, 0x58, 0x13, 0x3c, 0x7c, 0x1f, 0xba, 0xb3,
-	0x06, 0xfd, 0x39, 0x67, 0xdf, 0x01, 0x00, 0x00, 0xff, 0xff, 0x0a, 0x00, 0xb1, 0x5f, 0x49, 0x02,
-	0x00, 0x00,
+var fileDescriptor_viewer_b865312303b538d4 = []byte{
+	// 452 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x93, 0xdf, 0x6b, 0x13, 0x41,
+	0x10, 0xc7, 0xb3, 0xf7, 0x23, 0xe6, 0x46, 0x09, 0xc7, 0x52, 0x65, 0x29, 0x7d, 0x08, 0xf7, 0x14,
+	0x5a, 0xc8, 0x43, 0x84, 0xbe, 0x57, 0x0c, 0x8d, 0x12, 0x63, 0xd9, 0x16, 0xc1, 0x27, 0xb9, 0xf4,
+	0x96, 0xba, 0x26, 0xee, 0x85, 0xdb, 0xa5, 0x36, 0xfe, 0x25, 0xfe, 0x89, 0xea, 0x1f, 0xe0, 0xb3,
+	0xcc, 0xec, 0xed, 0x35, 0x42, 0x7c, 0xfb, 0xce, 0xdc, 0x67, 0xe6, 0x3b, 0x33, 0x9b, 0xc0, 0xb3,
+	0x7b, 0xad, 0xbe, 0xa9, 0x66, 0xb2, 0x6d, 0x6a, 0x57, 0x73, 0xf0, 0x51, 0xb5, 0xda, 0xae, 0x8a,
+	0x4f, 0x90, 0x5d, 0x36, 0xe5, 0xf6, 0xf3, 0xb2, 0xae, 0x14, 0x1f, 0x42, 0xa4, 0x2b, 0xc1, 0x46,
+	0x6c, 0x9c, 0xca, 0x48, 0x57, 0x9c, 0x43, 0x62, 0xca, 0xaf, 0x4a, 0x44, 0x23, 0x36, 0xce, 0x24,
+	0x69, 0xcc, 0x59, 0xfd, 0x5d, 0x89, 0x98, 0x28, 0xd2, 0xfc, 0x18, 0x06, 0xb7, 0xa5, 0x53, 0x77,
+	0x75, 0xb3, 0x13, 0x09, 0xb1, 0x5d, 0x5c, 0xcc, 0x5a, 0x83, 0x85, 0x36, 0x6b, 0x9e, 0x43, 0x6c,
+	0x9b, 0xdb, 0xd6, 0x01, 0x25, 0xb6, 0xab, 0x94, 0x75, 0x64, 0x91, 0x4a, 0xd2, 0x87, 0x2c, 0x0a,
+	0xd5, 0xb6, 0x79, 0x5d, 0xba, 0x92, 0x9f, 0x41, 0x6a, 0xea, 0x4a, 0x59, 0xc1, 0x46, 0xf1, 0xf8,
+	0xe9, 0xf4, 0xf9, 0xe4, 0x71, 0xa1, 0x49, 0xb7, 0x8d, 0xf4, 0x0c, 0xc2, 0x1b, 0x6d, 0xd6, 0x56,
+	0x44, 0xff, 0x81, 0x71, 0x32, 0xe9, 0x99, 0xe2, 0x04, 0x06, 0x6f, 0xaf, 0xdf, 0x2f, 0xc9, 0x05,
+	0x87, 0x75, 0x0d, 0x0d, 0x9b, 0x49, 0x94, 0xc5, 0x39, 0x0c, 0x17, 0xda, 0xa8, 0x6b, 0xd5, 0x68,
+	0x65, 0x89, 0x09, 0x17, 0x62, 0x7b, 0x17, 0xca, 0x21, 0xbe, 0x2f, 0x37, 0x64, 0x97, 0x4a, 0x94,
+	0xc5, 0x0f, 0x06, 0x03, 0x2c, 0xa4, 0x92, 0x13, 0xc8, 0x1e, 0x2e, 0x1e, 0xb4, 0xbd, 0xd9, 0x6d,
+	0x43, 0xdd, 0x63, 0xa2, 0xfb, 0x8a, 0x28, 0xb5, 0x08, 0x5f, 0x43, 0xed, 0xae, 0xab, 0x8d, 0x7d,
+	0x6d, 0x97, 0xe0, 0x53, 0xe8, 0x5b, 0x1a, 0x4d, 0x24, 0xb4, 0xea, 0xf1, 0xfe, 0xaa, 0xff, 0x0e,
+	0x2e, 0x5b, 0xb2, 0xf8, 0xc3, 0x00, 0x3e, 0x10, 0x45, 0x06, 0xa7, 0x90, 0xb8, 0x30, 0xd7, 0x70,
+	0xfa, 0x62, 0xbf, 0x81, 0xa7, 0xd0, 0x48, 0x12, 0xc3, 0x8f, 0x20, 0x75, 0xda, 0x6d, 0xc2, 0xcf,
+	0xc3, 0x07, 0x94, 0xad, 0xd7, 0xca, 0xb4, 0xe3, 0xf9, 0x80, 0x4f, 0x20, 0xbd, 0xc3, 0x5b, 0x8b,
+	0x9f, 0x4f, 0x46, 0xec, 0xe0, 0x2b, 0xa0, 0xfd, 0xbc, 0x27, 0x3d, 0xc6, 0xcf, 0x20, 0xf9, 0x62,
+	0x6b, 0x23, 0x7e, 0x79, 0xfc, 0x68, 0x1f, 0x0f, 0x0f, 0x34, 0xef, 0x49, 0x82, 0x10, 0xde, 0x68,
+	0xa3, 0xc4, 0xef, 0x03, 0x70, 0x38, 0x3b, 0xc2, 0x08, 0xbd, 0xea, 0x43, 0x52, 0x95, 0xae, 0x3c,
+	0x3d, 0x0f, 0x7b, 0xd3, 0xe9, 0x32, 0x48, 0x67, 0xef, 0xae, 0x6e, 0x3e, 0xe6, 0x3d, 0x94, 0x97,
+	0xf2, 0xe2, 0x6a, 0x9e, 0x33, 0x3e, 0x80, 0x04, 0xcd, 0xf2, 0x08, 0xd5, 0xe2, 0xcd, 0x72, 0x96,
+	0xc7, 0xab, 0x3e, 0xfd, 0x87, 0x5e, 0xfe, 0x0d, 0x00, 0x00, 0xff, 0xff, 0xb1, 0x74, 0x24, 0x3f,
+	0x53, 0x03, 0x00, 0x00,
 }
